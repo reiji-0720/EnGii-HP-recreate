6f286e00ca1cd09b1d1c89466dbcd932492656de
Hiroaki Yamada <54619750+vivaruckk17@users.noreply.github.com>
2020-06-11 01:18:19 +0900

    メディアフッター

---
+++ components/mediaFooter.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="mediaFooter">
+        <p>copyright 2020- EnGii all rights reserved</p>
+        <img src="~/assets/instagram.png" class="inataLogo">
+        <img src="~/assets/twitter.png" class="twitterLogo">
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+.mediaFooter {
+    display: flex;
+    align-items: center;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -30,12 +30,10 @@
 
 <script>
 import Logo from '~/components/Logo.vue'
-import Header from '~/components/Header.vue'
 
 export default {
   components: {
     Logo,
-    Header
   }
 }
 </script>
