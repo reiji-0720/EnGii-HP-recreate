cdbf306fb7c59fbaee3de0eadcccafcab6791ae9
tanaka-masato <tnkmasto09031999@gmail.com>
2020-06-11 01:56:06 +0900

    contact
    文字ずれる

--- components/contact.vue
+++ components/contact.vue
@@ -1,31 +1,37 @@
 <template>
-    <div class="contactContainer">
-        <h2 class="contactText">CONTACT</h2>
-        <div class="contactArea">
-            <div class="contact">
-                <nuxt-link to="">お気軽にお問い合わせください<span class="triangle"></span></nuxt-link>
-            </div>
-        </div>
+  <div class="contactContainer">
+    <h1 class="contactText">CONTACT</h1>
+    <div class="contactArea">
+      <nuxt-link to class="contact">
+        お気軽にお問い合わせください<span class="triangle"></span>
+      </nuxt-link>
     </div>
+  </div>
 </template>
 
 <script>
-export default {
-    
-}
+export default {};
 </script>
 
 <style>
-.contactContainer{
-
+.contactContainer {
 }
 
-.contactArea{
-    background-color: #E2F1F7;
-    height: 130px;
+.contactArea {
+  background-color: #e2f1f7;
+  height: 130px;
+	line-height: 130px;
 }
 
-.contact{
-    line-height: 130px;
+.contact {
+	display: inline-block;
+  vertical-align: middle;
+		line-height: 130px;
+
+  height: 60px;
+  width: 350px;
+  background-color: #66bad7;
+  text-decoration: none;
+  color: #FFFFFF;
 }
 </style>
