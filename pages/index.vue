f290e2b7ddfa1e30112bd48476958a50678e3222
Hiroaki Yamada <54619750+vivaruckk17@users.noreply.github.com>
2020-06-13 17:13:27 +0900

    Revert "Revert "Update index.vue""
    This reverts commit 77a2bc08570a7e74072467db69ad6a7cc1c4cb2a.

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,6 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        EnGii-HP-recreate
-      </h1>
-      <h2 class="subtitle">
-        My classy Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    
     <MediaFooter />
     <News />
     <div class="sreviceTitle">
