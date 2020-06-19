4063e4f331ea43c28556f531ae5d9ea4fa45db71
Hiroaki Yamada <54619750+vivaruckk17@users.noreply.github.com>
2020-06-19 14:10:30 +0900

    メディアリンク張りました

--- components/mediaFooter.vue
+++ components/mediaFooter.vue
@@ -3,8 +3,12 @@
     <div class="footerContainer">
       <p>copyright 2020- EnGii all rights reserved</p>
       <div class="mediaLogo">
-        <img src="~/assets/instagram.png" />
-        <img src="~/assets/twitter.png" />
+        <a href="https://www.instagram.com/engii_official">
+          <img src="~/assets/instagram.png" />
+        </a>
+        <a href="https://twitter.com/EnGii_official">
+          <img src="~/assets/twitter.png" />
+        </a>
       </div>
     </div>
   </div>
@@ -23,66 +27,65 @@ export default {};
   display: flex;
   align-items: center;
   position: relative;
- padding: 10px 0 7px 0;
+  padding: 10px 0 7px 0;
 }
 .mediaFooter p {
   color: #ffff;
   margin-left: 20px;
 }
 .mediaLogo {
-  position:absolute;
-  right:20px;
+  position: absolute;
+  right: 20px;
   align-items: center;
 }
-.mediaLogo img{
+.mediaLogo img {
   margin: 0 7px 0 7px;
 }
 
 @media screen and (min-width: 1001px) {
-
 }
 
 @media screen and (min-width: 731px) and (max-width: 1000px) {
-   .footerContainer {
-  display: flex;
-  align-items: center;
-  position: relative;
- padding: 7px 0 7px 0;
-}
-.footerContainer p {
-  color: #ffff;
-  font-size: 0.7rem;
-}
-.mediaLogo {
-  position:absolute;
-  right:7px;
-  align-items: center;
-}
-.mediaLogo img{
-  margin: 4px 5px 0 5px;
-  width: 25%;
-}
+  .footerContainer {
+    display: flex;
+    align-items: center;
+    position: relative;
+    padding: 7px 0 7px 0;
+  }
+  .footerContainer p {
+    color: #ffff;
+    font-size: 0.7rem;
+  }
+  .mediaLogo {
+    position: absolute;
+    right: 7px;
+    align-items: center;
+  }
+  .mediaLogo img {
+    margin: 4px 5px 0 5px;
+    width: 25%;
+  }
 }
 
 @media screen and (max-width: 730px) {
   .footerContainer {
-  display: flex;
-  align-items: center;
-  position: relative;
- padding: 7px 0 7px 0;
-}
-.footerContainer p {
-  color: #ffff;
-  font-size: 0.6rem;
-}
-.mediaLogo {
-  position:absolute;
-  right:7px;
-  align-items: center;
-}
-.mediaLogo img{
-  margin: 4px 5px 0 5px;
-  width: 20%;
-}
+    display: flex;
+    align-items: center;
+    position: relative;
+    padding: 7px 0 7px 0;
+  }
+  .footerContainer p {
+    color: #ffff;
+    font-size: 0.6rem;
+  }
+  .mediaLogo {
+    position: absolute;
+    right: 7px;
+    align-items: center;
+  }
+  .mediaLogo img {
+    margin: 4px 5px 0 5px;
+    width: 20%;
+  }
 }
 </style>
--- plugins/vue-scrollto.js
+++ plugins/vue-scrollto.js
@@ -4,5 +4,5 @@ import VueScrollTo from 'vue-scrollto'
 Vue.use(VueScrollTo, {
  duration: 1800,
  easing: [0, 0, 0.1, 1],
- offset: -110,
+ offset: -70,
 })
