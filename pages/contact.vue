f377d985381eadefe69a422b04da173a6ebdfada
REiJi <[shino327gh@gmail.com]>
2020-06-26 07:54:24 +0900

    Update contact.vue

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,6 +1,36 @@
 <template>
     <div class="main">
         <contactHeader />
+        <div class="cotactContainer">
+          <p class="explain">EnGiiについてご相談やご質問などがある方は<br>
+            下記の電話番号かお問い合わせフォームよりお問い合わせください。</p>
+          <div class="form">
+            <div class="formBox genre">
+              <p class="adjustP"></p>
+              <input type="text">
+            </div>
+            <div class="formBox name">
+              <p adjustP></p>
+              <input type="text">
+            </div>
+            <div class="formBox nameFurigana">
+              <p adjustP></p>
+              <input type="text">
+            </div>
+            <div class="formBox tel">
+              <p adjustP></p>
+              <input type="text">
+            </div>
+            <div class="formBox mail">
+              <p adjustP></p>
+              <input type="text">
+            </div>
+            <div class="formBox content">
+              <p adjustP></p>
+              <textarea type="text"></textarea>
+            </div>
+          </div>
+        </div>
     </div>
 </template>
 <script>
@@ -13,5 +43,15 @@ export default {
 };
 </script>
 <style scoped>
+.formBox{
+  display:flex;
+}
+.adjustP{
+  width:50%;
+}
+input,
+textarea{
+  width:40%;
+}
 
 </style>
