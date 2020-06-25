e78728390dd362b16ae62545e4794d8ba3a29aa7
REiJi <[shino327gh@gmail.com]>
2020-06-26 05:02:32 +0900

    contact

---
+++ components/contact/contact-header.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="main">
+        <div class="header">
+            <div class="box headerBoxLeft">
+                <div class="boxAdjust">
+                    <img src="~/assets/icon01-contact.png">
+                    <h1>CONTACT</h1>
+                </div>
+            </div>
+            <div class="box headerBoxRight">
+                <img src="~/assets/contact-main.jpg">
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {};
+</script>
+<style scoped>
+.header{
+    display:flex;
+}
+.box{
+    width:50%;
+}
+.boxAdjust{
+    width:50%;
+}
+.boxAdjust img{
+    width:50%;
+}
+
+.headerBoxRight img{
+    width:100%;
+}
+</style>
---
+++ pages/contact.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="main">
+        <contactHeader />
+    </div>
+</template>
+<script>
+import contactHeader from "~/components/contact/contact-header";
+
+export default {
+  components: {
+    contactHeader
+  }
+};
+</script>
+<style scoped>
+
+</style>
