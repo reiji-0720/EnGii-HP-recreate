9b2d8aa62bfa1a53a415bc5e9a77a65f51735c7e
reiji-0720 <55520463+reiji-0720@users.noreply.github.com>
2020-06-26 15:38:24 +0900

    modal作成

--- components/parts/contactModal.vue
+++ components/parts/contactModal.vue
@@ -1,11 +1,12 @@
 
 <template>
  <div class="modal">
-
+ <button class="modal" @click="$emit('modal')">
 
    <div class="modal-wrap">
     <div class="modal-thanks">
-    <p>回答ありがとうございました</p>
+    <p class="text-main">回答ありがとうございました</p>
+    <p class="subText">回答いただいた内容は無事送信されました</p>
     </div>
      <button class="btnCloseModal" @click="$emit('modal')"> 
        <div class="btn-close">
@@ -13,38 +14,79 @@
        </div>
      </button>
    </div>
+   </button>
  </div>
 </template>
 
 <style  scoped>
  .modal {
    position: fixed;
-   top: 100px;
+   top: 0;
    width: 100%;
-   height: 50vh;
+   height: 100vh;
+   background-color:rgba(170,170,170,0.7);
 
    z-index:100000;
  }
  .modal-wrap {
-     background-color:#3bb2db;
-     width:50%;
+     background-color:#EDF8FC;
+     width:500px;
      margin: 0 auto;
-     height: 100%;
+     height: 300px;
      text-align:center;
+     border-radius:5px;
+     border: solid 2px #3bb2db;
+}
+.text-main{
+    font-size:30px;
+    margin-bottom:10px;
+}
+.modal-thanks{
+    padding-top:80px;
+
 }
 .btnCloseModal{
-    margin-top:100px;
-    padding: 20px 0;
-    font-size:30px;
-    font-weight:600;
+    margin-top:50px;
+    padding: 10px 0;
+    font-size:20px;
+   
     cursor:pointer;
-    background-color:white;
-    border-radius: 15px;
+    background-color:#3bb2db;
+    border-radius: 5px;
+    color:white;
     border:none;
     width:50%;
 }
 .btn-close{
     border:none;
+}
+
+@media screen and (max-width: 600px){
+     .modal-wrap {
+        width:300px;
+    }
+    .text-main{
+        font-size:20px;
+        font-weight:600;
+        margin-bottom:10px;
+    }
+    .subText{
+        margin-top:20px;
+        font-size: 5px;
+        
+    }
+    .btnCloseModal{
+    margin-top:50px;
+    padding: 10px 0;
+    font-size:12px;
+   
+    cursor:pointer;
+    background-color:#3bb2db;
+    border-radius: 5px;
+    color:white;
+    border:none;
+    width:50%;
+}
 }
 
 </style>
