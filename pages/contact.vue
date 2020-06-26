7c84ee114d8e17413f2ac737ac36c9b9cc5bb2c3
reiji-0720 <55520463+reiji-0720@users.noreply.github.com>
2020-06-26 09:24:14 +0900

    contact

--- pages/contact.vue
+++ pages/contact.vue
@@ -6,28 +6,43 @@
             下記の電話番号かお問い合わせフォームよりお問い合わせください。</p>
           <div class="form">
             <div class="formBox genre">
-              <p class="adjustP"></p>
+              <div class="text">
+              <p class="adjustP">お問い合わせジャンル</p>
+              </div>
               <input type="text">
             </div>
             <div class="formBox name">
-              <p adjustP></p>
+            <div class="text">
+              <p adjustP>お名前</p>
+              </div>
               <input type="text">
             </div>
             <div class="formBox nameFurigana">
-              <p adjustP></p>
+            <div class="text">
+              <p adjustP>お名前（フリガナ）</p>
+              </div>
               <input type="text">
             </div>
             <div class="formBox tel">
-              <p adjustP></p>
+            <div class="text">
+              <p adjustP>電話番号</p>
+              </div>
               <input type="text">
             </div>
             <div class="formBox mail">
-              <p adjustP></p>
-              <input type="text">
+            <div class="text">
+              <p adjustP>メールアドレス</p>
+              </div>
+              <input placeholder="サンプルテキスト" type="text">
             </div>
             <div class="formBox content">
-              <p adjustP></p>
-              <textarea type="text"></textarea>
+            <div class="text">
+              <p adjustP>お問い合わせ内容</p>
+              </div>
+              <textarea rows="20"cols="60" ></textarea>
+            </div>
+            <div class="submit">
+              <button class="btn-submit bg-mainColor">送信</button>
             </div>
           </div>
         </div>
@@ -43,15 +58,103 @@ export default {
 };
 </script>
 <style scoped>
+.cotactContainer{
+  width:80%;
+  margin: 0 auto;
+}
+.explain{
+  margin-top:100px;
+  font-size:22px;
+  letter-spacing:0.2em;
+}
+.form{
+  margin-top:200px;
+}
 .formBox{
+  margin-bottom: 80px;
   display:flex;
+  width: 100%;
 }
 .adjustP{
+  width:100%;
+}
+.text{
   width:50%;
+  height:40px;
 }
 input,
 textarea{
+  margin:0 auto;
   width:40%;
+  float:right;
+  right:0;
+  float:top;
+  border:none;
+  background-color:#EDF8FC;
+}
+.textarea{
+
+}
+.submit{
+  text-align:center;
+  margin-bottom:100px;
+}
+.bg-mainColor{
+
+}
+.btn-submit {
+  padding: 10px;
+  text-align: center;
+  display: inline-block;
+  vertical-align: middle;
+  /* height: 60px; */
+  width: 159.89px;
+  text-decoration: none;
+  color: #ffffff;
+  font-size: 16px;
+  border:none;
 }
 
+@media screen and (max-width: 1000px) {
+  .btn-submit {
+    font-size: 13px;
+  }
+}
+@media screen (min-width:1366px){
+  .cotactContainer{
+    max-width:1366px;
+ 
+  }
+}
+@media screen and (max-width:1366px){
+  .explain{
+  margin-top:100px;
+  font-size:18px;
+  letter-spacing:0.2em;
+}
+}
+@media screen and (max-width:1000px){
+  .explain{
+  margin-top:100px;
+  font-size:13px;
+  letter-spacing:0.2em;
+}
+}
+@media screen and (max-width:730px){
+    .explain{
+  margin-top:100px;
+  font-size:8px;
+  letter-spacing:0.2em;
+}
+}
+@media screen and (max-width:500px){
+      .explain{
+  margin-top:100px;
+  font-size:5px;
+  letter-spacing:0.2em;
+}
+}
+@media screen and (max-width:400px){
+  
+}
 </style>
