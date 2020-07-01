d4a076f8c46331bad11c4fecd3c6bcbf3a2321d7
Hiroaki Yamada <54619750+vivaruckk17@users.noreply.github.com>
2020-07-02 01:26:52 +0900

    制作事例諸々Components導入

---
+++ components/example.vue
@@ -0,0 +1,27 @@
+<template>
+<div class="productionExampleContainer">
+    <div class="productionExample">
+        <p>制作事例</p>
+    </div>
+</div>
+</template>
+
+<style>
+
+    .productionExampleContainer {
+        text-align: center;
+        margin: auto;
+    }
+
+    .productionExample {
+        margin: auto;
+        background-color: #39acd8;
+        width: 15%;
+    }
+
+    .productionExample p{
+        font-size: 1.3rem;
+        color: #FFFF;
+        padding: 8px 0;
+    }
+</style>
---
+++ pages/application.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="applicationContainer">
+    <example class="examplePosition"/>
+    <appAchievement />
+</div>
+</template>
+
+<script>
+import appAchievement from "~/components/appAchievement.vue"
+import example from "~/components/example.vue"
+
+export default {
+  components: {
+    appAchievement,
+    example
+  }
+}
+</script>
+
+<style>
+
+.examplePosition {
+    margin: 10% auto;
+}
+</style>
