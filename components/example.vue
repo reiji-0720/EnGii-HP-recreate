06755edc9986ed5155f4d7afe008ca4153307a7f
Hiroaki Yamada <54619750+vivaruckk17@users.noreply.github.com>
2020-07-04 01:03:44 +0900

    アプリケーションデザインページ完成

--- components/example.vue
+++ components/example.vue
@@ -23,4 +23,38 @@
   color: #ffff;
   padding: 8px 0;
 }
+
+@media screen and (max-width: 1000px) {
+  .productionExample p {
+    font-size: 1rem;
+    color: #ffff;
+    padding: 8px 0;
+  }
+}
+
+@media screen and (max-width: 740px) {
+  .productionExample {
+    margin: auto;
+    background-color: #39acd8;
+    width: 30%;
+  }
+  .productionExample p {
+    font-size: 1.3rem;
+    color: #ffff;
+    padding: 8px 0;
+  }
+}
+
+@media screen and (max-width: 500px) {
+  .productionExample {
+    margin: auto;
+    background-color: #39acd8;
+    width: 35%;
+  }
+  .productionExample p {
+    font-size: 1.0rem;
+    color: #ffff;
+    padding: 8px 0;
+  }
+}
 </style>
